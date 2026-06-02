abe1996c6d6b85c305ddec0a82fd593e888af94f
Santigarrido28 <Santiagop2001@outlook.com>
2026-06-02 03:38:06 +0000

    First init

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+This module takes care of starting the API Server,
+Loading the DB and Adding the endpoints
 """
+
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-# from models import Person
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -29,14 +29,54 @@ def sitemap():
     return generate_sitemap(app)
 
 
+# GET all members
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+def get_members():
 
+    members = jackson_family.get_all_members()
+
+    return jsonify(members), 200
+
+
+# GET one member
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+
+    member = jackson_family.get_member(member_id)
+
+    if member is None:
+        return jsonify({"msg": "Member not found"}), 404
+
+    return jsonify(member), 200
+
+
+# POST new member
+@app.route('/members', methods=['POST'])
+def add_member():
+
+    body = request.get_json()
+
+    new_member = {
+        "first_name": body["first_name"],
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
+    }
+
+    created_member = jackson_family.add_member(new_member)
+
+    return jsonify(created_member), 200
+
+
+# DELETE member
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+
+    deleted = jackson_family.delete_member(member_id)
+
+    if deleted is False:
+        return jsonify({"msg": "Member not found"}), 404
+
+    return jsonify({"done": True}), 200
 
 
 # This only runs if `$ python src/app.py` is executed
